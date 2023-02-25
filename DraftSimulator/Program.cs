855d6f6d9dbf97e46077f3bef89e9f4b4522ce68
ryan.sihota <ryansihota@hotmail.com>
2023-02-25 13:29:55 -0800

    feat: add global exception handling to draft sim

--- DraftSimulator/Program.cs
+++ DraftSimulator/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<DraftStore>();
+builder.Services.AddSingleton<ExceptionHandlerMiddleware>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,8 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
