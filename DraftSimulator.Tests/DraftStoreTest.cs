eb4c71766086268d45200b53aedd829e3cf2452f
ryan.sihota <ryansihota@hotmail.com>
2023-03-19 14:51:24 -0700

    test: add draft store tests
    adding tests for creating and getting draft resources.
    
    updating github action to build and test draft sim when draft tests are changed.

--- DraftSimulator.Tests/DraftStoreTest.cs
+++ DraftSimulator.Tests/DraftStoreTest.cs
@@ -8,13 +8,90 @@ public class DraftStoreTest
 {
     Mock<ILogger<DraftStore>>? mockLogger;
 
-    [Fact]
-    public void Test1()
+    public DraftStoreTest()
     {
         mockLogger = new Mock<ILogger<DraftStore>>();
-        Assert.NotNull(mockLogger);
+    }
 
+    [Fact]
+    public void DraftCreatedSuccesfully()
+    {
+        // Arrange
+        NewDraft newDraft = new NewDraft()
+        {
+            Name = "MockDraft",
+            Type = "Hockey",
+            NumPlayers = 2,
+            Teams = new string[] { "Ryan", "Daniel" }
+        };
+
+        #nullable disable
         DraftStore draftStore = new DraftStore(mockLogger.Object);
         Assert.NotNull(draftStore);
+        #nullable enable
+
+        // Act
+        Draft mockDraft = draftStore.Add(newDraft);
+
+        // Assert
+        Assert.NotNull(mockDraft);
+        Assert.True(mockDraft.Name == newDraft.Name);
+        Assert.True(mockDraft.Type == newDraft.Type);
+        Assert.True(mockDraft.NumPlayers == newDraft.NumPlayers);
+        foreach(var team in mockDraft.Teams)
+        {
+            Assert.Contains(team.Name, newDraft.Teams);
+        }
+    }
+
+    [Fact]
+    public void GetDraftSuccesfully()
+    {
+        // Arrange
+        NewDraft newDraft = new NewDraft()
+        {
+            Name = "MockDraft",
+            Type = "Hockey",
+            NumPlayers = 2,
+            Teams = new string[] { "Ryan", "Daniel" }
+        };
+
+        #nullable disable
+        DraftStore draftStore = new DraftStore(mockLogger.Object);
+        Assert.NotNull(draftStore);
+        #nullable enable
+
+        // Act
+        Draft mockDraft = draftStore.Add(newDraft);
+        Assert.NotNull(mockDraft);
+        Draft mockDraftGet = draftStore.Get(mockDraft.DraftId);
+
+        // Assert
+        Assert.True(mockDraft == mockDraftGet);
+    }
+
+    [Fact]
+    public void GetDraftFailure()
+    {
+        // Arrange
+        NewDraft newDraft = new NewDraft()
+        {
+            Name = "MockDraft",
+            Type = "Hockey",
+            NumPlayers = 2,
+            Teams = new string[] { "Ryan", "Daniel" }
+        };
+        
+        #nullable disable
+        DraftStore draftStore = new DraftStore(mockLogger.Object);
+        Assert.NotNull(draftStore);
+        #nullable enable
+
+        // Act
+        Draft mockDraft = draftStore.Add(newDraft);
+        Assert.NotNull(mockDraft);
+
+        // Assert
+        Assert.Throws<DraftNotFound>(() => draftStore.Get(Guid.NewGuid()));
     }
 }
